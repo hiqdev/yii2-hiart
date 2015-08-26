b1266afab73ed7d8deb418d4c99806d74099ee4a
Andrii Vasyliev <sol@hiqdev.com>
2015-08-26 16:28:56 +0000

    minor

--- src/HiArtException.php
+++ src/HiArtException.php
@@ -18,6 +18,6 @@ class HiArtException extends \yii\db\Exception
      */
     public function getName()
     {
-        return 'HiPanel active record exception';
+        return 'HiArt active record exception';
     }
 }
