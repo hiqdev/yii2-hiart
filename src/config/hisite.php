e1aab41511290928c2cb00cafc4a3b176a56ef22
Andrii Vasyliev <sol@hiqdev.com>
2017-01-24 10:07:15 +0000

    fixed configs again

---
+++ src/config/hisite.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Tools to use API as ActiveRecord for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-hiart
+ * @package   yii2-hiart
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
+
+return require './common.php';
