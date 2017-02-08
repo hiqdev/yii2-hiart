8d26e37a95e76a7b90cc0acfb94b7b165e54a005
Andrii Vasyliev <sol@hiqdev.com>
2017-02-08 09:25:38 +0000

    added functional test for simple config (no DI)

---
+++ tests/functional/SimpleConfigTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace hiqdev\hiart\tests\functional;
+
+use hiqdev\hiart\curl\Request;
+use hiqdev\hiart\rest\Connection;
+use Yii;
+use yii\console\Application;
+
+class SimpleConfigTest extends \PHPUnit\Framework\TestCase
+{
+    protected $simpleConfig = [
+        'id' => 'hiart-simple-config',
+        'basePath' => __DIR__,
+        'components' => [
+            'hiart' => [
+                'class' => Connection::class,
+                'requestClass' => Request::class,
+                'baseUri' => 'https://site.com/api/v3/',
+            ],
+        ],
+    ];
+
+    private $app;
+    private $container;
+
+    public function setUp()
+    {
+        $this->app = Yii::$app;
+        $this->container = Yii::$container;
+        Yii::$app = new Application($this->simpleConfig);
+        Yii::$container = new yii\di\Container();
+    }
+
+    public function tearDown()
+    {
+        Yii::$app = $this->app;
+        Yii::$container = $this->container;
+    }
+
+    public function testGetDb()
+    {
+        $db = Connection::getDb();
+        $this->assertInstanceOf(Connection::class, $db);
+        $this->assertSame(Request::class, $db->requestClass);
+    }
+}
