d7a000e74b28f2617bd173f7be57aa2b0057286f
Andrii Vasyliev <sol@hiqdev.com>
2017-03-30 10:09:39 +0000

    added phpunit 5 <-> 6 compatibility dirty hack

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -13,6 +13,22 @@ error_reporting(-1);
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+/// PHPUNIT 5 <-> 6 compatibility dirty hack
+foreach ([
+    \PHPUnit_Framework_TestCase::class => \PHPUnit\Framework\TestCase::class,
+    \PHPUnit_Framework_Constraint::class => \PHPUnit\Framework\Constraint\Constraint::class,
+] as $old => $new) {
+    foreach ([$old => $new, $new => $old] as $one => $other) {
+        if (!class_exists($one) && class_exists($other)) {
+            $pos = strrpos($one, '\\');
+            $class = $pos === FALSE ? $one : substr($one, $pos+1);
+            $space = $pos === FALSE ? '' : substr($one, 0, $pos);
+            $namespace = $space ? "namespace $space;" : '';
+            eval("${namespace}abstract class $class extends \\$other {};\n");
+        }
+    }
+}
+
 use hiqdev\composer\config\Builder;
 use yii\console\Application;
 
