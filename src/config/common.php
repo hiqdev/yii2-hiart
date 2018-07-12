ab38cbb2dcba98affa146f799be1d6ea2e649e9a
Andrii Vasyliev <sol@hiqdev.com>
2018-07-12 22:56:57 +0300

    fixed config for new DI

--- src/config/common.php
+++ src/config/common.php
@@ -9,30 +9,25 @@
  */
 
 return empty($params['hiart.enabled']) ? [] : [
-    'modules' => array_filter([
-        'debug' => empty($params['debug.enabled']) ? null : [
-            'panels' => [
-                'hiart' => [
-                    'class' => \hiqdev\hiart\debug\DebugPanel::class,
+    'app' => [
+        'modules' => array_filter([
+            'debug' => empty($params['debug.enabled']) ? null : [
+                'panels' => [
+                    'hiart' => [
+                        'class' => \hiqdev\hiart\debug\DebugPanel::class,
+                    ],
                 ],
             ],
-        ],
-    ]),
-    'components' => array_filter([
-        $params['hiart.dbname'] => array_filter([
-            'class'         => $params['hiart.class'],
-            'requestClass'  => $params['hiart.requestClass'],
-            'name'          => $params['hiart.dbname'],
-            'auth'          => $params['hiart.auth'],
-            'config'        => $params['hiart.config'],
-            'baseUri'       => $params['hiart.baseUri'],
         ]),
-    ]),
-    'container' => [
-        'singletons' => [
-            \hiqdev\hiart\ConnectionInterface::class => function () {
-                return Yii::$app->get(Yii::$app->params['hiart.dbname']);
-            },
-        ],
     ],
+
+    \hiqdev\hiart\ConnectionInterface::class => \yii\di\Reference::to($params['hiart.dbname']),
+    $params['hiart.dbname'] => array_filter([
+        'class'         => $params['hiart.class'],
+        'requestClass'  => $params['hiart.requestClass'],
+        'name'          => $params['hiart.dbname'],
+        'auth'          => $params['hiart.auth'],
+        'config'        => $params['hiart.config'],
+        'baseUri'       => $params['hiart.baseUri'],
+    ]),
 ];
