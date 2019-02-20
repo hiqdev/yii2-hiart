c1fac46d31063c966307d2d6b39de7eba0af52af
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2019-02-20 15:10:40 +0200

    csfixed

--- src/auto/Request.php
+++ src/auto/Request.php
@@ -5,7 +5,7 @@
  * @link      https://github.com/hiqdev/yii2-hiart
  * @package   yii2-hiart
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\hiart\auto;
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,8 +5,9 @@
  * @link      https://github.com/hiqdev/yii2-hiart
  * @package   yii2-hiart
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2018, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2019, HiQDev (http://hiqdev.com/)
  */
+
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
