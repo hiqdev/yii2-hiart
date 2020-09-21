802c16084e21d2351d37be23e77324b64ebfb7e5
Yurii Myronchuk <bladeroot@hiqdev.com>
2020-09-21 15:52:21 +0000

    use parent

--- src/ExtendedCollection.php
+++ src/ExtendedCollection.php
@@ -53,7 +53,7 @@ class ExtendedCollection extends Collection
     protected function performOperation(string $command, array $data, array $queryOptions = []) : array
     {
         if ($this->isConsistent()) {
-            return $this->first->batchQuery($command, $data, $queryOptions);
+            return parent::performOperation($command, $data, $queryOptions);
         }
 
         foreach ($data as $key => $record) {
