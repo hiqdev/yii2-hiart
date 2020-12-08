8326e3063dd3ab52c652633e91a6232d867fea42
Andrey Klochok <andreyklochok@gmail.com>
2020-12-08 18:31:09 +0200

    Prevent extral `load models` when LinkPager render, cache total count

--- src/ActiveDataProvider.php
+++ src/ActiveDataProvider.php
@@ -10,6 +10,10 @@
 
 namespace hiqdev\hiart;
 
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\QueryInterface;
+
 class ActiveDataProvider extends \yii\data\ActiveDataProvider
 {
     /**
@@ -27,9 +31,19 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
      */
     public bool $countSynchronously = false;
 
+    /**
+     * @var bool|mixed
+     */
+    private bool $loadModels = true;
+
     public function enableSynchronousCount(): void
     {
         $this->countSynchronously = true;
+    }
+
+    public function preventLoadModels(): void
+    {
+        $this->loadModels = false;
     }
 
     /**
@@ -41,6 +55,33 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
      */
     protected function prepareTotalCount(): int
     {
-        return $this->countSynchronously ? parent::prepareTotalCount() : PHP_INT_MAX;
+        if ($this->countSynchronously) {
+            $app = Yii::$app;
+
+            return $app->getCache()->getOrSet([$app->user->getId(), $this->query->modelClass, __METHOD__], fn(): int => parent::prepareTotalCount(), 5);
+        }
+
+        return PHP_INT_MAX;
+    }
+
+    /** {@inheritdoc} */
+    protected function prepareModels(): array
+    {
+        if (!$this->query instanceof QueryInterface) {
+            throw new InvalidConfigException('The "query" property must be an instance of a class that implements the QueryInterface e.g. yii\db\Query or its subclasses.');
+        }
+        $query = clone $this->query;
+        if (($pagination = $this->getPagination()) !== false) {
+            $pagination->totalCount = $this->getTotalCount();
+            if ($pagination->totalCount === 0) {
+                return [];
+            }
+            $query->limit($pagination->getLimit())->offset($pagination->getOffset());
+        }
+        if (($sort = $this->getSort()) !== false) {
+            $query->addOrderBy($sort->getOrders());
+        }
+
+        return $this->loadModels ? $query->all($this->db) : array_pad([], $pagination->pageSize, 0);
     }
 }
